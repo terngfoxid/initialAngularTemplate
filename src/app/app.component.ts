c8d0928dee9a2ec9af8a5be98b5e411e551e2e88
terngfoxid <terngfoxid@live.jp>
2024-06-18 15:40:00 +0700

    feat(all): first push project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu'
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule,MatCardModule,MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
